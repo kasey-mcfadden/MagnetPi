35d7052d3b40a870213b42d842468d9f67504393
Kasey McFadden <kaseysmcfadden@gmail.com>
2020-07-31 11:48:42 -0400

    Update vid.py

--- vid.py
+++ vid.py
@@ -1,18 +1,19 @@
 import cv2
-import numpy as np
 import time
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)  # the pin numbers refer to the board connector not the chip
+GPIO.setmode(GPIO.BOARD)  # the pin numbers refer to the board connector
 cam = cv2.VideoCapture(0) # initialize the camera
 
-MAGNET = 16
-pathOut = 'video.avi'
-fps = 0.5
-size = (1920,1080)
+MAGNET = 16               # pin number on raspberry pi GPIO
+pathOut = 'video.mp4'     # output file name for video
+fps = 2.0                 # frames per second to write to video
+size = (1920,1080)        # dimensions of video
+time_limit = 60           # time limit, in seconds
 
 GPIO.setup(MAGNET, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up pin as an input with a pull-up resistor
-out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
 tic = time.perf_counter()
+i = 1
 
 while True:
     toc = time.perf_counter()
@@ -26,8 +27,9 @@ while True:
             # cv2.destroyWindow('SnapshotTest')
             # cv2.imwrite('SnapshotTest.jpg',image)
             out.write(image)
-            print("Image captured")
-    if (toc - tic) >= 30:
+            i += 1
+            print("Image captured (%d)" % i)
+    if (toc - tic) >= time_limit:
         break
     
 out.release()
